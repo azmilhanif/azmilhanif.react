dd4e43b95ba50d3e73dc508d9c14fcffb733ad1b
Azmil Alirupendi <Azmil412@gmail.com>
2023-12-19 18:47:13 +0800

    change fontstyle and added introduction message in index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+// Created by azmilhanif(hazel)
+
 import './App.css';
+import React from 'react'
+
+
 
 function App() {
+
+
   return (
     <div>
-      
     </div>
   );
 }
+
+
 
 export default App;
